11f247064866578eb006d60465c49b5f78ac96e4
Valentin Brémont <valentin.bremont@gmail.com>
2023-10-30 14:39:27 +0100

    create build folder script OK

--- build_zip.sh
+++ build_zip.sh
@@ -1,4 +1,9 @@
 #! /bin/sh
 
+
+if [ ! -d ""./build"" ]; then 
+    mkdir build
+fi
+
 zip -r build/memento.zip . -x .gitignore README.md build_zip.sh .git/\*
-nemo /home/val/Developpement/BrowserExtensions/Memento/build
+nemo ./build
